b508c778d2257d7298b06a75f4d434cefb3f7513
Technici4n <13494793+Technici4n@users.noreply.github.com>
2021-04-17 23:07:45 +0200

    Clarify that it can be used for living entities, and that modification should be deferred

--- fabric-item-api-v1/src/main/java/net/fabricmc/fabric/api/item/v1/FabricElytraFlyingItem.java
+++ fabric-item-api-v1/src/main/java/net/fabricmc/fabric/api/item/v1/FabricElytraFlyingItem.java
@@ -23,13 +23,17 @@ import net.minecraft.item.ElytraItem;
 import net.minecraft.item.ItemStack;
 
 /**
- * An interface that allows implementing items to enable elytra flight for players, when equipped in the chest equipment slot.
+ * An interface that allows implementing items to enable elytra flight for living entities, when equipped in the chest equipment slot.
  * While being used, the item will be damaged <b>every 20 ticks</b> through {@link ItemStack#damage(int, LivingEntity, Consumer)}.
  */
 public interface FabricElytraFlyingItem {
 	/**
 	 * Returns {@code true} if this item allows flight based on its stack and user, and {@code false} otherwise.
-	 * This function will be called when a player attempts to start elytra flight, and will also be called every tick during elytra flight to make sure it is still allowed.
+	 * This function will be called when an entity attempts to start elytra flight,
+	 * and will also be called every tick during elytra flight to make sure it is still allowed.
+	 *
+	 * <p>It may be called multiple times in a row in some cases, so you should not modify the stack directly,
+	 * but rather check every tick if your item is equipped and {@linkplain LivingEntity#isFallFlying() the entity is fall flying}.
 	 *
 	 * @param stack the stack for the elytra like item
 	 * @param user  the user of the elytra like
